0cc4cb8608e0f0c092e4310d3c79aa38472589c2
Ryan Fallah <ryan_fallah@hotmail.com>
2019-10-15 10:01:45 +1100

    Week 5 solutions

--- Week 5/q3.s
+++ Week 5/q3.s
@@ -6,30 +6,40 @@ Address       Data Definition
 0x10010010         .word 5
 0x10010014         .word 7
 
-la   $t0, aa
-
-
-lw   $t0, bb
-
-
 lb   $t0, bb
 # 666 in Hex is 00 00 02 9A
+# $t0 = 00 or 9A
+# 00 = 1000 0000
+# becomes 0000 .... 0000
+# 9A = 1001 1010
+# mips will pad 1111 1111 1111 1111 1111 1111 1001 1010
 
+la   $t0, aa
+# $t0 = 0x10010000
+
+lw   $t0, bb
+# $t0 = 666
 
 lw   $t0, aa+4
-
+# $t0 = 666
 
 la   $t1, cc
 lw   $t0, ($t1)
-
+# $t1 = 0x10010008
+# $t0 = 1
 
 la   $t1, cc
 lw   $t0, 8($t1)
+# $t1 = 0x10010008
+# $t0 = 5
 
 
 li   $t1, 8
 lw   $t0, cc($t1)
-
+# $t1 = 8
+# $t0 = 5
 
 la   $t1, cc
 lw   $t0, 2($t1)
+#$t1 = 0x10010008
+# crashes
--- Week 5/q8.s
+++ Week 5/q8.s
@@ -1,8 +1,31 @@
     .data
+numbers: .word 0 1 2 3 4 5 6 7 8 9
 
     .text
 main:
 
+    li $t0, 0 # i = 0
+while:
+    bge $t0, 5, end_while # while (i < 5) {
 
+    mul $t1, $t0, 4
+    lw $t3, numbers($t1) #x
+
+    li $t2, 9
+    sub $t2, $t2, $t0 # $t2 = 9 - i
+    mul $t2, $t2, 4
+    lw $t4, numbers($t2) #y
+
+    # $t1 = 4*i
+    # $t2 = 4*(9-i)
+    # $t3 = x
+    # $t4 = y
+
+    sw $t4, numbers($t1) # numbers[i] = y;
+    sw $t3, numbers($t2) # numbers[9 - i] = x;
+
+    addi $t0, $t0, 1 # i++
+    j while
+end_while:
 
     jr $ra
