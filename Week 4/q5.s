bc6cb6b2adb90e1b63ffd50612b48c15690ae974
Ryan Fallah <ryan_fallah@hotmail.com>
2020-06-23 12:15:54 +1000

    Week 4 tute

--- Week 4/q5.s
+++ Week 4/q5.s
@@ -1,10 +1,23 @@
 .data
-msg: .asciiz "Coming soon...\n"
+
 
 .text
 main:
-    li $v0, 4
-    la $a0, msg
-    syscall
+    
+    li $t0, 24 # x = 24
+for1:
+    bge $t0, 42, end_for1
+
+    li $v0, 1
+    move $a0, $t0
+    syscall # printf("%d")
+
+    li $v0, 11
+    li $a0, 10 # '\n'
+    syscall # putchar('\n')
+
+    addi $t0, $t0, 3 # x = x + 3
+    b for1
+end_for1:
 
     jr $ra # return
