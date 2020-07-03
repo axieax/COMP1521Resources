882aa358bb340ba0d7b3f94a42a4045c48a67a19
Ryan Fallah <ryan_fallah@hotmail.com>
2020-07-04 01:50:37 +1000

    more comments q56

--- Week 5/q5_6.s
+++ Week 5/q5_6.s
@@ -12,7 +12,7 @@ while1:
     bge $t0, 10, end_while1 # if (i >= 10) goto end_while
 
     li $v0, 5
-    syscall # scanf
+    syscall # scanf("%d")
 
     # NOW, the scanned integer is inside $v0
 
@@ -23,18 +23,18 @@ while1:
     j while1
 end_while1:
 
-    li $s0, 42
+    li $s0, 42 # Load $s0 for fun, just an example of what you can do if you wish to preserve registers across function calls.
 
     la $a0, numbers
     jal print_numbers # print_numbers(numbers)
     # notice!! jal overwrites $ra when you call the function
 
-
     # whats value of $s0 here???
+    # even though print_numbers overwrote $s0 in its logic (line 50), it is backed up to the stack on line 48 and restored on line 78 so the value of $s0 remains 42.
 
     # clean up stack frame
     lw   $ra, 0($sp)   # restore $ra
-    addi $sp, $sp, 4   # restore sp
+    addi $sp, $sp, 4   # restore sp (opposite of line 7)
 
     li $v0, 0
     jr $ra # return 0
@@ -45,7 +45,7 @@ print_numbers:
 
     addi $sp, $sp, -8  # create stack frame
     sw   $ra, 0($sp)   # save return address
-    sw   $s0, 4($sp)   # save return address
+    sw   $s0, 4($sp)   # save $s0
    
     move $s0, $a0 # move $a0 into $s0 so we dont overwrite when doing syscalls
     # note im using $s0 for fun, could have just used a $t register
@@ -75,8 +75,8 @@ end_while2:
     syscall # putchar(' ')
 
     # clean up stack frame
-    lw   $s0, 4($sp)
+    lw   $s0, 4($sp)   # restore $s0
     lw   $ra, 0($sp)   # restore $ra
-    addi $sp, $sp, 8   # restore sp
+    addi $sp, $sp, 8   # restore sp (opposite of line 46)
 
     jr  $ra # return
