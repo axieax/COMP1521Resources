32418fbec09f368737ab8a63fcbe3879f5befa42
Ryan Fallah <ryan_fallah@hotmail.com>
2020-09-15 10:18:57 +1000

    Week 1

--- Week 1/q4.c
+++ Week 1/q4.c
@@ -5,5 +5,38 @@ int main(void)
 {
     char str[N] = {'H', 'i', '\0'};
     printf("%s\n", str);
+
+    int i;
+    while (i < 10) {
+
+        i++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        
+    }
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int f(int n) { if (n == 1) { return n; } }
--- Week 1/recursive.c
+++ Week 1/recursive.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+void f(int* nums, int i) {
+    if (i == 10) {
+        return;
+    }
+
+    printf("%d\n", nums[i]);
+    f(nums, i+1);
+}
+
 int main()
 {
     int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d\n", nums[i]);
-    }
+    f(nums, 0);
 
     return 0;
 }
